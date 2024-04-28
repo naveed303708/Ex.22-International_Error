56314b4a1207c772c4cc0506b157601b7168c5d2
Naveed Ahmed Farooqui <162836180+naveed303708@users.noreply.github.com>
2024-04-29 01:03:46 +0500

    Add files via upload

---
+++ International_Error.js
@@ -0,0 +1,9 @@
+"use strict";
+//If you haven't received an array index error in one of your programs yet,try to make one happen.
+//creating array
+let newArray = ['will smith', 'james bond', 'tom cruise'];
+// accessing array by inde
+console.log(newArray[5]);
+//Make sure you correct the error before closing the program.
+//correct the error
+console.log(newArray);
---
+++ International_Error.ts
@@ -0,0 +1,14 @@
+
+//If you haven't received an array index error in one of your programs yet,try to make one happen.
+
+//creating array
+let newArray :string[] = ['will smith', 'james bond', 'tom cruise'];
+
+// accessing array by inde
+console.log(newArray[5]);
+
+
+//Make sure you correct the error before closing the program.
+
+//correct the error
+console.log(newArray);
